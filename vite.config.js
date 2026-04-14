c97e457d487733ba65c093210dbb68311e4abd62
Taylorisms15 <mark@taylorisms.com>
2026-04-14 12:56:00 -0400

    Disable minifySyntax to fix esbuild TDZ bug

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  },
+  esbuild: {
+    minifyIdentifiers: true,
+    minifySyntax: false,
+    minifyWhitespace: true,
+  }
 })
